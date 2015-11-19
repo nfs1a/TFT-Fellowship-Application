6c14337c2b5c14d028a3df630e2e238512f4e358
Tako Tsai <nfs1atako@gmail.com>
2015-11-19 23:58:05 +0800

    測試mail功能

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,3 +16,22 @@
 
 Route::get('dashboard','PagesController@dashboard');
 Route::get('basic','PagesController@basic');
+
+Route::get('mail_test', function()
+{
+    // 傳送給郵件view的變數資料
+    $template_data = array(
+        'name'=> 'Tako'
+    );
+    // 收件者資料
+    $userinfo = array(
+        'email'=>'nfs1atako@gmail.com',
+        'subject'=>'歡迎使用 Laravel Mail!'
+    );
+    // 寄送郵件，使用use方法將資料從外部傳送給匿名函式使用
+    Mail::send('emails.test', $template_data, function($message) use ($userinfo)
+    {
+        $message->to('nfs1atako@gmail.com')->subject('歡迎使用 Laravel Mail!');
+    });
+    echo '完成寄送!!';
+});
