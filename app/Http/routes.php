701857bf0845e1865680ad32e3625922cba5d8eb
Tako Tsai <nfs1atako@gmail.com>
2015-11-24 04:06:07 +0800

    update for basic form

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,19 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-//Route::get('/','WelcomeController@index');
-//Route::get('contact','WelcomeController@contact');
-
-
 Route::get('dashboard','PagesController@dashboard');
 Route::get('basic','PagesController@basic');
 
@@ -31,7 +17,8 @@ Route::get('mail_test', function()
     // 寄送郵件，使用use方法將資料從外部傳送給匿名函式使用
     Mail::send('emails.test', $template_data, function($message) use ($userinfo)
     {
-        $message->to('nfs1atako@gmail.com')->subject('歡迎使用 Laravel Mail!');
+        $message->to('yianccc@gmail.com')->subject('歡迎使用 Laravel Mail!');
     });
     echo '完成寄送!!';
 });
+
