95bd0dfdfd6ab41f36681820898cf1a8ddedd801
v123582 <v123582@gmail.com>
2015-11-26 04:29:25 +0800

    update layout path

--- resources/views/layouts/_default.blade.php
+++ resources/views/layouts/_default.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>Teach For India Fellowship 2016</title>
-    <link rel="stylesheet" href="/bootstrap.min.css">
-    <link rel="stylesheet" href="/font-awesome.min.css">
-    <link rel="stylesheet" href="/final.min.css">
-    <link rel="stylesheet" href="/bootstrap-additions.css">
-    <link rel="stylesheet" href="/bootstrap-social.css"><!--Load Css filters-->
-    <link rel="stylesheet" href="/style.css"><!--fallback if cdn fails to load-->
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/final.min.css">
+    <link rel="stylesheet" href="/css/bootstrap-additions.css">
+    <link rel="stylesheet" href="/css/bootstrap-social.css"><!--Load Css filters-->
+    <link rel="stylesheet" href="/css/style.css"><!--fallback if cdn fails to load-->
     
 </head>
 <body>
