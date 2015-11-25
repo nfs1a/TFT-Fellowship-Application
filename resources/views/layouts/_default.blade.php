4b4788c05116ae14fc6513ee90795e37b013cac9
Tako Tsai <nfs1atako@gmail.com>
2015-11-26 04:43:20 +0800

    修改nav圖片

--- resources/views/layouts/_default.blade.php
+++ resources/views/layouts/_default.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <title>Teach For India Fellowship 2016</title>
+    <title>Teach For Taiwan Fellowship 2016</title>
     <link rel="stylesheet" href="/bootstrap.min.css">
     <link rel="stylesheet" href="/font-awesome.min.css">
     <link rel="stylesheet" href="/final.min.css">
