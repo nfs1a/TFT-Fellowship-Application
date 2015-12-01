fbae330c0b42830f929bc0b479f700c1a0324611
v123582 <v123582@gmail.com>
2015-12-02 01:10:19 +0800

    update experienceSection content

--- resources/views/teachExperience/create.blade.php
+++ resources/views/teachExperience/create.blade.php
@@ -11,66 +11,155 @@
                 <div class="row">
                     <div class="sectionPan panel panel-default col-md-7 shadow">
                         <div class="panel-body">
-                            <div>
-                                請填寫你過往重要的工作或社團經驗，至多三項，每題至多200字。
-                            </div> 
-                            @for ($i = 0; $i < 3; $i++)
-                                
 
-                                <!-- Professional Experience 1 -->
+                            <div>            
+                                <!-- title -->
+                                <div class="legend">
+                                    教師資格
+                                </div> 
                                 <div>
-                                    <!-- title -->
-                                    <div class="legend">
-                                        Professional Experience {{ $i }}
-                                        <hr class="small-top">
-                                    </div> 
-                                    <!-- form content -->
-                                    <div class="form-group" onmouseover="displayExample('organization')">
-                                        <label for="input" class="col-md-2 control-label">組織單位</label>
+                                    請勾選你已認證之教師資格。
+                                    <hr class="small-top">
+                                </div> 
+                                <!-- form content -->
+                                <div class="form-group" onmouseover="displayExample('certification')">
+                                    <label for="input" class="col-md-2 control-label">幼教教師證</label>
 
-                                        <div class="col-md-10">
-                                            <input type="text" class="form-control" name="organization[]" placeholder="組織單位"
-                                                   value="{{ Input::old('organization')[$i] }}" required="required" maxlength="20">
-                                            {{ $errors->first('organization.'.$i) }}
-                                        </div>
-                                    </div>
-                                    <div class="form-group" onmouseover="displayExample('position')">
-                                        <label for="input" class="col-md-2 control-label">職稱</label>
-
-                                        <div class="col-md-10">
-                                            <input type="text" class="form-control" name="position[]" placeholder="王大明"
-                                                   value="{{ Input::old('position')[$i] }}" required="required" maxlength="20">
-                                            {{ $errors->first('position.'.$i) }}
-                                        </div>
-                                    </div>
-                                    <div class="form-group" onmouseover="displayExample('startendDate')">
-                                        <label for="input" class="col-md-2 control-label">起迄時間</label>
-
-                                        <div class="col-md-5">
-                                            <input type="text" name="startDate[]" class="form-control date" placeholder="1991/01/33"
-                                                required="required" value="{{ Input::old('startDate')[$i] }}" />
-                                            {{ $errors->first('startDate.'.$i) }}
-                                        </div>
-                                         <div class="col-md-5">
-                                            <input type="text" name="endDate[]" class="form-control date" placeholder="1991/01/33"
-                                                required="required" value="{{ Input::old('endDate')[$i] }}" />
-                                            {{ $errors->first('endDate.'.$i) }}
-                                        </div>
-                                    </div>
-                                    <div class="form-group" onmouseover="displayExample('description')">
-                                        <label for="input" class="col-md-2 control-label">簡述成就</label>
-                                        <div class="col-md-10">
-                                            <textarea name="description[]" class="form-control" cols="40" 
-                                                value="" placeholder="簡述成就">{{ Input::old('description')[$i] }}</textarea>
-                                            {{ $errors->first('description.'.$i) }}
-                                        </div>
-                                    </div>
+                                    <label class="radio-inline">
+                                      <input type="radio" name="preschool" value="0">
+                                        無
+                                    </label>
+                                    <label class="radio-inline">
+                                      <input type="radio" name="preschool" value="1">
+                                        已修畢教程，未取得
+                                    </label>
+                                    <label class="radio-inline">
+                                      <input type="radio" name="preschool" value="2">
+                                        已修畢教程，預計今年取得
+                                    </label>
+                                    <label class="radio-inline">
+                                      <input type="radio" name="preschool" value="3">
+                                        已取得
+                                    </label>
                                 </div>
+                                <div class="form-group" onmouseover="displayExample('certification')">
+                                    <label for="input" class="col-md-2 control-label">小學教師證</label>
 
-                            @endfor
-                                                           
-   
+                                    <label class="radio-inline">
+                                      <input type="radio" name="elementary" value="0">
+                                        無
+                                    </label>
+                                    <label class="radio-inline">
+                                      <input type="radio" name="elementary" value="1">
+                                        已修畢教程，未取得
+                                    </label>
+                                    <label class="radio-inline">
+                                      <input type="radio" name="elementary" value="2">
+                                        已修畢教程，預計今年取得
+                                    </label>
+                                    <label class="radio-inline">
+                                      <input type="radio" name="elementary" value="3">
+                                        已取得
+                                    </label>
+                                </div>
+                                <div class="form-group" onmouseover="displayExample('certification')">
+                                    <label for="input" class="col-md-2 control-label">中學教師證</label>
 
+                                    <label class="radio-inline">
+                                      <input type="radio" name="secondary" value="0">
+                                        無
+                                    </label>
+                                    <label class="radio-inline">
+                                      <input type="radio" name="secondary" value="1">
+                                        已修畢教程，未取得
+                                    </label>
+                                    <label class="radio-inline">
+                                      <input type="radio" name="secondary" value="2">
+                                        已修畢教程，預計今年取得
+                                    </label>
+                                    <label class="radio-inline">
+                                      <input type="radio" name="secondary" value="3">
+                                        已取得
+                                    </label>
+                                </div>
+                                <div class="form-group" onmouseover="displayExample('certification')">
+                                    <label for="input" class="col-md-2 control-label">特教教師證</label>
+
+                                    <label class="radio-inline">
+                                      <input type="radio" name="special" value="0">
+                                        無
+                                    </label>
+                                    <label class="radio-inline">
+                                      <input type="radio" name="special" value="1">
+                                        已修畢教程，未取得
+                                    </label>
+                                    <label class="radio-inline">
+                                      <input type="radio" name="special" value="2">
+                                        已修畢教程，預計今年取得
+                                    </label>
+                                    <label class="radio-inline">
+                                      <input type="radio" name="special" value="3">
+                                        已取得
+                                    </label>
+                                </div>
+                            </div>
+                        
+                            <div>            
+                                <!-- title -->
+                                <div class="legend">
+                                    教學或服務經驗
+                                </div> 
+                                <div>
+                                    請填寫你過往教學或志工服務經驗，至多三項，每題至多200字。
+                                    <hr class="small-top">
+                                </div> 
+                                @for ($i = 0; $i < 3; $i++)
+                                    <div>
+                                        <!-- form content -->
+                                        <div class="form-group" onmouseover="displayExample('organization')">
+                                            <label for="input" class="col-md-2 control-label">組織單位</label>
+
+                                            <div class="col-md-10">
+                                                <input type="text" class="form-control" name="organization[]" placeholder="組織單位"
+                                                       value="{{ Input::old('organization')[$i] }}" required="required" maxlength="20">
+                                                {{ $errors->first('organization.'.$i) }}
+                                            </div>
+                                        </div>
+                                        <div class="form-group" onmouseover="displayExample('position')">
+                                            <label for="input" class="col-md-2 control-label">職稱</label>
+
+                                            <div class="col-md-10">
+                                                <input type="text" class="form-control" name="position[]" placeholder="王大明"
+                                                       value="{{ Input::old('position')[$i] }}" required="required" maxlength="20">
+                                                {{ $errors->first('position.'.$i) }}
+                                            </div>
+                                        </div>
+                                        <div class="form-group" onmouseover="displayExample('startendDate')">
+                                            <label for="input" class="col-md-2 control-label">起迄時間</label>
+
+                                            <div class="col-md-5">
+                                                <input type="text" name="startDate[]" class="form-control date" placeholder="1991/01/33"
+                                                    required="required" value="{{ Input::old('startDate')[$i] }}" />
+                                                {{ $errors->first('startDate.'.$i) }}
+                                            </div>
+                                             <div class="col-md-5">
+                                                <input type="text" name="endDate[]" class="form-control date" placeholder="1991/01/33"
+                                                    required="required" value="{{ Input::old('endDate')[$i] }}" />
+                                                {{ $errors->first('endDate.'.$i) }}
+                                            </div>
+                                        </div>
+                                        <div class="form-group" onmouseover="displayExample('description')">
+                                            <label for="input" class="col-md-2 control-label">簡述成就</label>
+                                            <div class="col-md-10">
+                                                <textarea name="description[]" class="form-control" cols="40" 
+                                                    value="" placeholder="簡述成就">{{ Input::old('description')[$i] }}</textarea>
+                                                {{ $errors->first('description.'.$i) }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <hr class="small-top">
+                                @endfor
+                            </div>
                         </div>
                     </div>
                     @include('partials/_hint')
@@ -98,6 +187,10 @@
         if (dom == 'description') {
             exampleHelpText = '請簡述在該單位任內成就';
             exampleText = '簡述';
+        }
+        if (dom == 'certification') {
+            exampleHelpText = '請勾選你已認證之教師資格';
+            exampleText = '勾選';
         }
         document.getElementById("exampleHelpText").innerHTML = exampleHelpText;
         document.getElementById("exampleText").innerHTML = exampleText;
