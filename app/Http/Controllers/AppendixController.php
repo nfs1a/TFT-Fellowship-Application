1f7975e6fe59828c02c5c56c04f5aebac1c85c8f
v123582 <v123582@gmail.com>
2015-12-06 23:18:41 +0800

    update AppendixController: fileUpload

--- app/Http/Controllers/AppendixController.php
+++ app/Http/Controllers/AppendixController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AppendixRequest;
 use Log;
+use Input;
 
 class AppendixController extends Controller
 {
@@ -14,13 +15,32 @@ class AppendixController extends Controller
         return view('appendix.create');
     }
 
+    public function fileUpload($fileName)
+    {
+        if(Input::hasFile($fileName)){
+            $file = Input::file($fileName);
+            $extension = $file->getClientOriginalExtension();
+            $file_name = strval(time()).str_random(5).'.'.$extension;
+            $destination_path = public_path().'/user-upload/';
+            $upload_success = $file->move($destination_path, $file_name);
+            $fileUrl = '/user-upload/'. $file_name;
+            return $fileUrl;
+        }
+        else{
+            return false;
+        }
+    } 
+
     public function store(AppendixRequest $request)
     {
         $input = $request->all();
         Log::info('-------- AppendixController: store --------');
-        Log::info($input);
-        Log::info('--------');
 
+        foreach ($input as $key => $value) {
+            $fileUrl = AppendixController::fileUpload($key);
+            Log::info($key . ' => ' . $fileUrl);
+        }
+        Log::info('-------- AppendixController: end --------');
         return redirect('/dashboard');
     }
 
