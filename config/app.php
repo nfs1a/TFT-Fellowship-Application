42108f397d8ce44d3f4a308a6825e68785ab54c5
ChihWeiLee <rm4xu6@yahoo.com.tw>
2015-12-07 20:14:03 +0800

    revert app_key and remove fb script in account view

--- config/app.php
+++ config/app.php
@@ -80,7 +80,7 @@ return [
 
     'key' => env('APP_KEY', 'SomeRandomStringWith32Characters'),
 
-    'cipher' => 'AES-128-CBC',
+    'cipher' => 'AES-256-CBC',
 
     /*
     |--------------------------------------------------------------------------
--- resources/views/account/account.blade.php
+++ resources/views/account/account.blade.php
@@ -19,23 +19,6 @@
 </head>
 
 <body>
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '948077125272735',
-      xfbml      : true,
-      version    : 'v2.5'
-    });
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
     <div id="container">
         <div id="navbar">
             <nav role="navigation" class="navbar navbar-fixed-top navbar-default background_white">
