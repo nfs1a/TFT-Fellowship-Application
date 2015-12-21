d3d6d5e37af4522c9ba095348146bc6c9bbf375b
蔡小蔡 <Mac@caixiaocais-MacBook-Pro.local>
2015-12-21 19:47:01 +0800

    fix basic bug

--- app/Http/Controllers/PreviewController.php
+++ app/Http/Controllers/PreviewController.php
@@ -15,7 +15,7 @@ class PreviewController extends Controller
         $user = User::with('work', 'teach', 'essay', 'basic', 'appendix', 'work.workExperiences', 'work.expertises', 'teach.teachExperiences')->get()->find(Auth::user()->id);
 
         $loginUser = Auth::check() ? Auth::user()->name : null;
-        $data = compact('loginUser', 'user');
+        return $data = compact('loginUser', 'user');
 		return view('preview.index',$data);
     }
 
