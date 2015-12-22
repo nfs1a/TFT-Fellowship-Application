de09dca8f9bd33f40522df944a7715376edceb63
v123582 <v123582@gmail.com>
2015-12-22 08:34:32 +0800

    update Middleware to route

--- app/Http/Controllers/PreviewController.php
+++ app/Http/Controllers/PreviewController.php
@@ -18,11 +18,11 @@ class PreviewController extends Controller
 		return view('preview.index',$data);
     }
 
-    public function index_()
+    public function indexApi()
     {
-        $user = User::with('work', 'teach', 'essay', 'basic', 'appendix', 'work.workExperiences', 'work.expertises', 'teach.teachExperiences','basic.eduHistorys')->get()->find(Auth::user()->id);
-        $loginUser = Auth::check() ? Auth::user()->name : null;
-        return $data = compact('loginUser', 'user');
+        $users = User::with('work', 'teach', 'essay', 'basic', 'appendix', 'work.workExperiences', 'work.expertises', 'teach.teachExperiences','basic.eduHistorys')->get()->toArray();
+        $data = compact('users');
+        return $data;
     }
 
 }
